2170b4ff7e662da22371e21a3cb27bb542e15d68
caitlin-rich <caitlinboagrich@gmail.com>
2021-08-19 12:54:54 -0400

    first commit

---
+++ client/components/game_components/index.js
@@ -0,0 +1,6 @@
+import 'phaser';
+import React from 'react';
+import MainScene from './scenes/MainScene'
+import OpeningScene from './scenes/OpeningScene'
+
+//Game building / class Game extends etc. goes here

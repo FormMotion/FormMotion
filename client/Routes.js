60bda72b086912d4c13ff31e58bf651b1ac8fa5b
kathleenconnaghan <77138134+kathleenconnaghan@users.noreply.github.com>
2021-08-23 16:20:05 -0400

    add platform and prize drawing canvases

--- client/Routes.js
+++ client/Routes.js
@@ -6,6 +6,7 @@ import About from './components/About';
 import Welcome from './components/Welcome';
 import Drawing from './components/drawing_components/Drawing.js';
 import Game from './components/Game';
+import Platform from './components/drawing_components/Platform.js';
 
 class Routes extends Component {
   constructor() {
@@ -20,6 +21,7 @@ class Routes extends Component {
           <Route path="/about" component={About} />
           <Route path="/play" component={Drawing} />
           <Route path="/game" component={Game} />
+          <Route path="/platform" component={Platform} />
         </Switch>
       </div>
     );

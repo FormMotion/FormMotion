0785b90353a5a938ccbd89ce9491485f976fe46b
ndmetrick <82462717+ndmetrick@users.noreply.github.com>
2021-08-25 12:45:50 -0500

    saving work

--- client/components/drawing_components/oneDrawing.js
+++ client/components/drawing_components/oneDrawing.js
@@ -28,17 +28,21 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 let sketchpad = null;
-let graph_paper = 'assets/graph-paper.png';
+let canvas_image = 'assets/graph-paper.png';
+
+const images = {
+  0: 'assets/graph-paper.png',
+  1: 'assets/temp_char_facing_left_run.png',
+  2: 'assets/temp_char_facing_right_run.png',
+  3: 'assets/eyeChar.png',
+  4: 'assets/surpriseBox.jpeg',
+};
 
 const oneDrawing = (props) => {
   const classes = useStyles();
   const [color, setColor] = useState('#aabbcc');
-  const [drawnChar, setDrawnChar] = useState(true);
+  const [defaultChar, setDefaultChar] = useState(0);
   const [thickness, setThickness] = useState(7);
-
-  if (!drawnChar) {
-    graph_paper = 'assets/eyeChar.png';
-  }
 
   useEffect(() => {
     if (sketchpad === null) {
@@ -60,7 +64,7 @@ const oneDrawing = (props) => {
   }
 
   function chooseMode(e) {
-    if (drawnChar) {
+    if (!defaultChar) {
       sketchpad.mode = e.target.value;
     }
   }
@@ -77,28 +81,85 @@ const oneDrawing = (props) => {
     document.body.removeChild(link);
   }
 
-  const handleExport = (e) => {
-    e.preventDefault();
-    if (drawnChar) {
-      const uri = sketchpad.toImage();
-      localStorage.setItem('playerDrawnCharacter', uri);
-    } else {
-      localStorage.setItem('playerDrawnCharacter', false);
+  const chooseDrawOrDefault = (e) => {
+    let choice = e.target.value;
+    // if the user chooses 0, they're choosing to draw.
+    // set defaultChar to 0 and put the graph paper image in
+    // and allow them to draw
+    if (choice === '0') {
+      setDefaultChar(0);
+      sketchpad.mode = 'draw';
+      canvas_image = images[choice];
     }
-    props.history.push('./platform');
+    // if the user chooses to use a default character, set the default,
+    // disable the drawing and clear the sketchpad, and set the
+    // appropraite image as the canvas image (random is a surprise box image)
+    if (choice > 0) {
+      setDefaultChar(choice);
+      sketchpad.clear();
+      sketchpad.mode = 'disabled';
+      console.log('my choice looks like this', images[choice]);
+      canvas_image = images[choice];
+    }
   };
 
-  const useDefaultCharacter = (e) => {
-    e.preventDefault();
-    sketchpad.clear();
-    setDrawnChar(false);
-    sketchpad.mode = 'disabled';
-  };
+  function setDataUrl(src, callback) {
+    const img = new Image();
+    img.crossOrigin = 'Anonymous';
+    img.onload = () => {
+      const canvas = document.createElement('canvas');
+      const ctx = canvas.getContext('2d');
+      let dataURL;
+      canvas.height = img.naturalHeight;
+      canvas.width = img.naturalWidth;
+      ctx.drawImage(img, 0, 0);
+      dataURL = canvas.toDataURL();
+      console.log(dataURL, 'dataUrl');
+      callback(dataURL);
+    };
+    img.src = src;
+    if (img.complete || img.complete === undefined) {
+      img.src =
+        'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==';
+      img.src = src;
+    }
+  }
 
   const drawCharacter = (e) => {
     e.preventDefault();
-    setDrawnChar(true);
+    setDefaultChar(0);
     sketchpad.mode = 'draw';
+  };
+
+  // get a random number between 1 and 3
+  const getRandomChar = () => {
+    return Math.random() * 3 + 1;
+  };
+
+  const handleExport = (e) => {
+    e.preventDefault();
+    // if the user hasn't chosen a default character and they've drawn on the canvas,
+    // put their drawing in local storage
+    if (!defaultChar && sketchpad.isDirty()) {
+      console.log('i got here to image');
+      const uri = sketchpad.toImage();
+      localStorage.setItem('playerDrawnCharacter', uri);
+
+      // if the user hasn't chosen a default character OR drawn on the canvas,
+      // choose a random default character and put it in local storage
+    } else {
+      let choice;
+      if (!sketchpad.isDirty() && !defaultChar) {
+        choice = getRandomChar();
+      } else {
+        choice = defaultChar;
+      }
+      console.log(choice, 'here is my choice');
+      setDataUrl(choice, (dataURL) => {
+        localStorage.setItem('playerDrawnCharacter', dataURL);
+      });
+    }
+    props.history.push('./platform');
   };
 
   const saveToGame = async (e) => {
@@ -117,23 +178,39 @@ const oneDrawing = (props) => {
           style={{
             borderStyle: 'solid',
             borderColor: 'black',
-            backgroundImage: `url(${graph_paper})`,
+            backgroundImage: `url(${canvas_image})`,
             backgroundSize: 'cover',
             backgroundPosition: 'center',
             backgroundRepeat: 'no-repeat',
           }}
         ></canvas>
       </Box>
-      {drawnChar && (
+      <Typography>Draw or choose pre-drawn character</Typography>
+      <FormControl className={classes.formControl}>
+        <NativeSelect
+          onChange={chooseDrawOrDefault}
+          className={classes.selectEmpty}
+        >
+          <option value={0}>Draw character</option>
+          <option value={1}>Eyes</option>
+          <option value={2}>Flamingo</option>
+          <option value={3}>Other</option>
+          <option value={4}>Be surprised!</option>
+        </NativeSelect>
+        <FormHelperText>
+          Draw, choose one of the provided options, or be surprised!
+        </FormHelperText>
+      </FormControl>
+      {/* {defaultChar && (
         <Button variant="contained" onClick={useDefaultCharacter}>
           Use default character
         </Button>
       )}
-      {!drawnChar && (
+      {!defaultChar && (
         <Button variant="contained" onClick={drawCharacter}>
           Draw character
         </Button>
-      )}
+      )} */}
       <Button variant="contained" onClick={downloadDrawing}>
         Download image to my local computer
       </Button>
@@ -156,12 +233,7 @@ const oneDrawing = (props) => {
       <br />
       <Typography>Mode</Typography>
       <FormControl className={classes.formControl}>
-        <NativeSelect
-          onChange={chooseMode}
-          name="age"
-          className={classes.selectEmpty}
-          // inputProps={{ 'aria-label': 'age' }}
-        >
+        <NativeSelect onChange={chooseMode} className={classes.selectEmpty}>
           <option value={'draw'}>Draw</option>
           <option value={'fill'}>Fill</option>
           <option value={'erase'}>Erase</option>

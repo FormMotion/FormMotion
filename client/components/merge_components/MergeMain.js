5a58efc2bf5857e39cb78d260338bc6b4313028e
caitlin-rich <caitlinboagrich@gmail.com>
2021-08-26 17:42:31 -0400

    adds jumping forward pose

--- client/components/merge_components/MergeMain.js
+++ client/components/merge_components/MergeMain.js
@@ -30,7 +30,7 @@ export default function StandingAvatar() {
     mergeImages([
 
       //BACKGROUND
-      { src: 'assets/transparent_background_500_x_800.png', x: 0, y: 0 },
+      { src: 'assets/transparent_background_800_x_800.png', x: 0, y: 0 },
       //HEAD
       { src: head, x: 130, y: 0 },
       //TORSO
@@ -45,8 +45,8 @@ export default function StandingAvatar() {
       { src: armLeftUpper, x: 20, y: 150 },
       { src: armLeftLower, x: 20, y: 300 },
       //Right ARM (from user perspective)
-      { src: armRightUpper, x: 400, y: 150 },
-      { src: armRightLower, x: 400, y: 320 },
+      { src: armRightUpper, x: 400, y: 170 },
+      { src: armRightLower, x: 400, y: 350 },
     ]).then((res) => setAvatar(res));
 
 
@@ -131,7 +131,7 @@ export function Landing() {
     mergeImages([
 
       //BACKGROUND
-      { src: 'assets/transparent_background_500_x_800.png', x: 0, y: 0 },
+      { src: 'assets/transparent_background_800_x_800.png', x: 0, y: 0 },
       //HEAD
       { src: head, x: 130, y: 0 },
       //TORSO
@@ -152,6 +152,100 @@ export function Landing() {
   }
 
   console.log('LANDING:', avatar)
+
+  return <div></div>;
+}
+
+
+//Jumping Forward and Up (positive x movement, negative y movement)
+export function JumpForwardAndUp() {
+
+  const head = localStorage.getItem('playerDrawnhead');
+  const torso = localStorage.getItem('playerDrawntorso');
+  const armRightUpper = localStorage.getItem('playerDrawnrightUpperArm');
+  const armRightLower = localStorage.getItem('playerDrawnrightLowerArm');
+  const armLeftUpper = localStorage.getItem('playerDrawnleftUpperArm');
+  const armLeftLower = localStorage.getItem('playerDrawnleftLowerArm');
+  const legRightUpper = localStorage.getItem('playerDrawnrightUpperLeg');
+  const legRightLower = localStorage.getItem('playerDrawnrightLowerLeg');
+  const legLeftUpper = localStorage.getItem('playerDrawnleftUpperLeg');
+  const legLeftLower = localStorage.getItem('playerDrawnleftLowerLeg');
+
+  //avatar is final merged image 
+  const [avatar, setAvatar] = useState(null);
+
+  //delete line if not using
+  const [rotatedHead, setRotatedHead] = useState(false);
+  const [rotatedTorso, setRotatedTorso] = useState(false);
+  const [rotatedArmRightUpper, setRotatedArmRightUpper] = useState(false);
+  const [rotatedArmRightLower, setRotatedArmRightLower] = useState(false);
+  const [rotatedArmLeftUpper, setRotatedArmLeftUpper] = useState(false);
+  const [rotatedArmLeftLower, setRotatedHeadArmLeftLower] = useState(false);
+  const [rotatedLegRightUpper, setRotatedLegRightUpper] = useState(false);
+  const [rotatedLegRightLower, setRotatedLegRightLower] = useState(false);
+  const [rotatedLegLeftUpper, setRotatedLegLeftUpper] = useState(false);
+  const [rotatedLegLeftLower, setRotatedLeftLower] = useState(false);
+
+
+  const rotate = (base64info, degrees, callback) => {
+    const canvas = document.createElement('canvas');
+    let ctx = canvas.getContext('2d');
+    let image = new Image();
+
+    image.src = base64info;
+
+    image.onload = function () {
+      canvas.width = degrees % 180 === 0 ? image.width + 50 : image.height + 50; 
+      canvas.height = degrees % 180 === 0 ? image.height + 50 : image.width + 50; 
+
+      ctx.translate(canvas.width / 2, canvas.height / 2); 
+      ctx.rotate((degrees * Math.PI) / 180); 
+      ctx.drawImage(image, image.width / -2, image.height / -2); 
+
+      //exports base64 
+      callback(canvas.toDataURL());
+    };
+  };
+
+  rotate(legRightUpper, 270, function (resultBase64) {
+    setRotatedLegRightUpper(resultBase64);
+  });
+
+  rotate(armRightUpper, 315, function(resultBase64) {
+    setRotatedArmRightUpper(resultBase64)
+  })
+
+  rotate(armRightLower, 180, function(resultBase64) {
+    setRotatedArmRightLower(resultBase64)
+  })
+
+
+
+  if (rotatedLegRightUpper && rotatedArmRightUpper && rotatedArmRightLower) {
+    mergeImages([
+
+      //BACKGROUND
+      { src: 'assets/transparent_background_800_x_800.png', x: 0, y: 0 },
+      //HEAD
+      { src: head, x: 130, y: 0 },
+      //TORSO
+      { src: torso, x: 120, y: 150 },
+      //Left LEG (from user perspective)
+      { src: legLeftUpper, x: 150, y: 400 },
+      { src: legLeftLower, x: 150, y: 580 },
+      //Right LEG (from user perspective)
+      { src: rotatedLegRightUpper, x: 290, y: 325 },
+      { src: legRightLower, x: 430, y: 400 },
+      //Left ARM (from user perspective)
+      { src: armLeftUpper, x: 20, y: 150 },
+      { src: armLeftLower, x: 20, y: 300 },
+      //Right ARM (from user perspective)
+      { src: rotatedArmRightUpper, x: 350, y: 150 },
+      { src: rotatedArmRightLower, x: 460, y: 85 },
+    ]).then((res) => setAvatar(res));
+  }
+
+  console.log(avatar)
 
   return <div></div>;
 }
@@ -194,8 +288,8 @@ export function Landing() {
 //     image.src = base64info;
 
 //     image.onload = function () {
-//       canvas.width = degrees % 180 === 0 ? image.width : image.height; 
-//       canvas.height = degrees % 180 === 0 ? image.height : image.width; 
+//       canvas.width = degrees % 180 === 0 ? image.width + 50 : image.height + 50; 
+//       canvas.height = degrees % 180 === 0 ? image.height + 50 : image.width + 50; 
 
 //       ctx.translate(canvas.width / 2, canvas.height / 2); 
 //       ctx.rotate((degrees * Math.PI) / 180); 
@@ -214,9 +308,9 @@ export function Landing() {
 //     mergeImages([
 
 //       //BACKGROUND
-//       { src: 'assets/transparent_background_500_x_800.png', x: 0, y: 0 },
+//       { src: 'assets/transparent_background_800_x_800.png', x: 0, y: 0 },
 //       //HEAD
-//       { src: rotatedHead, x: 130, y: 0 },
+//       { src: head, x: 130, y: 0 },
 //       //TORSO
 //       { src: torso, x: 120, y: 150 },
 //       //Left LEG (from user perspective)

ee1f67765e223e044a548813b61ae3e9e41b9a4e
caitlin-rich <caitlinboagrich@gmail.com>
2021-08-27 14:40:53 -0400

    fixes phaser refresh bug

--- client/components/Game.js
+++ client/components/Game.js
@@ -1,21 +1,12 @@
 import React, { useState } from "react";
 import PhaserGame from "./game_components/index.js";
 import { CircularProgress } from "@material-ui/core";
-import StandingAvatar from "./merge_components/MergeMain";
-import LandingAvatar from "./merge_components/Landing";
-import ForwardMovement from "./merge_components/ForwardMovement";
-import DownwardMovement from "./merge_components/DownwardMovement.js";
+
 
 export default function Game() {
   return (
     <div>
-      //These need to move to Platform component to prevent refresh issue
-            <StandingAvatar />
-      <LandingAvatar />
-      <ForwardMovement />
-      <DownwardMovement />
-    
-      <PhaserGame />
+ <PhaserGame />
     </div>
   );
 }

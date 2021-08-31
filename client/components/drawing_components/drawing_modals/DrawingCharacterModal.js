683c316a444276a1d148bcb0bba85702381c3518
Julia Crooijmans <juliacrooijmans@gmail.com>
2021-08-31 17:36:39 -0400

    seperated drawing component into two

---
+++ client/components/drawing_components/drawing_modals/DrawingCharacterModal.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles((theme) => ({
+    modal: {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    paper: {
+        backgroundColor: theme.palette.background.paper,
+        border: '2px solid #000',
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+        fontFamily: ["Roboto", "sans-serif"].join(","),
+        fontWeight: 300,
+    },
+}));
+
+export default function DrawingCharacterModal() {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
+    return (
+        <div>
+            <Button style={{ backgroundColor: '#86995a', margin: 10 }} variant='contained' type='button' onClick={handleOpen}>
+                Need help?
+            </Button>
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                    <div className={classes.paper}>
+                        <h2 id="transition-modal-title">On this page you draw your own character!</h2>
+                        <p id="transition-modal-description">Insert instructions here</p>
+                    </div>
+                </Fade>
+            </Modal>
+        </div>
+    );
+}

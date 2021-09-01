27c188d4420d6fa59653e895cbc0509556029aed
ndmetrick <82462717+ndmetrick@users.noreply.github.com>
2021-08-31 21:39:33 -0500

    trying to figure out the canvas

--- client/components/drawing_components/DrawPlatform.js
+++ client/components/drawing_components/DrawPlatform.js
@@ -1,17 +1,18 @@
-import React, { useState, useEffect } from "react";
-const Atrament = require("atrament");
-import DrawingTools from "./DrawingTools";
-import NavBar from "../NavBar";
+import React, { useState, useEffect } from 'react';
+const Atrament = require('atrament');
+import DrawingTools from './DrawingTools';
+import DefaultOptions from './DefaultOptions';
+import NavBar from '../NavBar';
 
-import StandingAvatar from "../merge_components/MergeMain";
-import LandingAvatar from "../merge_components/Landing";
-import ForwardMovement from "../merge_components/ForwardMovement";
-import JumpingMovement from "../merge_components/JumpingMovement.js";
+import StandingAvatar from '../merge_components/MergeMain';
+import LandingAvatar from '../merge_components/Landing';
+import ForwardMovement from '../merge_components/ForwardMovement';
+import JumpingMovement from '../merge_components/JumpingMovement.js';
 
 // material-ui
-import Grid from "@material-ui/core/Grid";
+import Grid from '@material-ui/core/Grid';
 
-let canvas_image = "assets/graph-paper.png";
+let canvas_image = 'assets/graph-paper.png';
 
 let platform = null;
 let prize = null;
@@ -22,13 +23,13 @@ const canvases = {
 };
 
 const names = {
-  platform: "platform",
-  prize: "prize",
+  platform: 'platform',
+  prize: 'prize',
 };
 
-const DrawPlatform = props => {
+const DrawPlatform = (props) => {
   useEffect(() => {
-    Object.keys(canvases).forEach(canvas => {
+    Object.keys(canvases).forEach((canvas) => {
       if (canvases[canvas] === null) {
         let currentCanvas = document.querySelector(`#${canvas}`);
         canvases[canvas] = new Atrament(currentCanvas);
@@ -44,20 +45,27 @@ const DrawPlatform = props => {
         direction="row"
         justifyContent="space-evenly"
         alignItems="flex-start"
-        spacing={2}
       >
+        <Grid Item>
+          <DrawingTools
+            canvases={canvases}
+            names={names}
+            history={props.history}
+            type="platformAndPrize"
+          />
+        </Grid>
         <Grid Item>
           <canvas
             id="platform"
             width="250"
             height="75"
             style={{
-              borderStyle: "solid",
-              borderColor: "black",
+              borderStyle: 'solid',
+              borderColor: 'black',
               backgroundImage: `url(${canvas_image})`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              backgroundRepeat: 'no-repeat',
             }}
           ></canvas>
         </Grid>
@@ -67,23 +75,23 @@ const DrawPlatform = props => {
             width="100"
             height="100"
             style={{
-              borderStyle: "solid",
-              borderColor: "black",
+              borderStyle: 'solid',
+              borderColor: 'black',
               backgroundImage: `url(${canvas_image})`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-              backgroundRepeat: "no-repeat",
+              backgroundSize: 'cover',
+              backgroundPosition: 'center',
+              backgroundRepeat: 'no-repeat',
             }}
           ></canvas>
         </Grid>
-        <div>
-          <DrawingTools
+        <Grid Item>
+          <DefaultOptions
             canvases={canvases}
-            names={names}
             history={props.history}
+            names={names}
             type="platformAndPrize"
           />
-        </div>
+        </Grid>
         {/* This is here so that the merged avatars are loaded into localStorage before we get to the game */}
         <StandingAvatar />
         <LandingAvatar />
--- client/components/game_components/index.js
+++ client/components/game_components/index.js
@@ -5,31 +5,52 @@ import OpeningScene from './scenes/OpeningScene';
 import PauseScene from './scenes/PauseScene';
 
 export default class Game extends React.Component {
-    componentDidMount() {
-        const config = {
-            type: Phaser.AUTO,
-            width: 1200,
-            height: 800,
-            physics: {
-                default: 'arcade',
-                arcade: {
-                    gravity: { y: 550 },
-                    debug: false,
-                },
-            },
-            scale: {
-                parent: 'game-container',
-                autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
-            },
-            scene: [MainScene, OpeningScene, PauseScene],
-        };
-        new Phaser.Game(config);
+  componentDidMount() {
+    const config = {
+      type: Phaser.AUTO,
+      width: 1200,
+      height: 800,
+      className: 'canvas',
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 550 },
+          debug: true,
+        },
+      },
+      scale: {
+        // mode: Phaser.Scale.FIT,
+        parent: 'game-container',
+        autoCenter: Phaser.Scale.CENTER_HORIZONTALLY,
+      },
+      scene: [MainScene, OpeningScene, PauseScene],
+    };
+
+    function resize() {
+      const canvas = document.querySelector('canvas');
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      const wratio = width / height;
+      const ratio = Number(config.width) / Number(config.height);
+      if (wratio < ratio) {
+        canvas.style.width = width + 'px';
+        canvas.style.height = width / ratio + 'px';
+      } else {
+        canvas.style.width = height * ratio + 'px';
+        canvas.style.height = height + 'px';
+      }
     }
-    shouldComponentUpdate() {
-        return false;
-    }
-    //Why do we need this line? 
-    render() {
-        return <div id="phaser-game" />;
-    }
+
+    window.onload = function () {
+      new Phaser.Game(config);
+      resize();
+      window.addEventListener('resize', resize, false);
+    };
+  }
+  shouldComponentUpdate() {
+    return false;
+  }
+  render() {
+    return <div id="phaser-game" />;
+  }
 }

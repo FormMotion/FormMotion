dc6e13e13970a4e119e3485c7d9106bff97af314
caitlin-rich <caitlinboagrich@gmail.com>
2021-09-01 12:17:23 -0400

    platforms move horizontally in addition to vertically

--- client/components/game_components/index.js
+++ client/components/game_components/index.js
@@ -12,8 +12,8 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 550 },
-      debug: true,
+      gravity: { y: 500 },
+      debug: false,
     },
   },
   scale: {

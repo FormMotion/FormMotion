a2b38c423cc43e3dca3850bb6c7ec4294631ec37
ndmetrick <82462717+ndmetrick@users.noreply.github.com>
2021-09-02 11:33:56 -0500

    Create index.js

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('*', function (req, res, next) {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  console.error(err.stack);
+  res
+    .status(err.status || 500)
+    .send(err.message || "Oops! There's an error on our end.");
+});
+
+module.exports = app;

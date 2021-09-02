a9143e7ccb08752a6cad883ea4c83eeb33b54d83
ndmetrick <82462717+ndmetrick@users.noreply.github.com>
2021-09-02 11:59:46 -0500

    Update start.js

--- start.js
+++ start.js
@@ -1,10 +1,10 @@
-const { db } = require('./server/db/index');
+
 
 const app = require('./server/index.js');
 const port = process.env.PORT || 3000;
 
 db.sync().then(() => {
-  console.log('db synced');
+
   app.listen(port, () => {
     console.log(`listening on port ${port}`);
   });
